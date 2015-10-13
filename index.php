a716968f46a9092743562b507e6dc2ffb863904f
Darazdeblek <vgagnieux@aol.com>
2015-10-14 01:11:46 +0200

    Ajout d'une ossature de base dans index.php
    Liaison des fichiers style.css, global.js et importation librairie jquery

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+	<title>Projet code</title>
+</head>
 
+<body>
 
+	<div id="container">
+	
 
+		
 
+	
+	</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>	
+<script type="text/javascript" src="js/global.js"></script>	
+</body>
+</html>
